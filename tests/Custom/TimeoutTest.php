c62968ea593c0c420feff35f5d454eb311509318
Christian Sciberras <christian@sciberras.me>
2023-06-14 22:08:15 +0200

    Improve timeouts test

--- tests/Custom/TimeoutTest.php
+++ tests/Custom/TimeoutTest.php
@@ -4,6 +4,7 @@ namespace Mink\WebdriverClassDriver\Tests\Custom;
 
 use Behat\Mink\Exception\DriverException;
 use Behat\Mink\Tests\Driver\TestCase;
+use Mink\WebdriverClassDriver\WebdriverClassicDriver;
 
 class TimeoutTest extends TestCase
 {
@@ -20,22 +21,24 @@ class TimeoutTest extends TestCase
         }
 
         // Reset the array of timeouts to avoid impacting other tests
-        $session->getDriver()->setTimeouts([]);
+        $this->getDriver()->setTimeouts([]);
 
         parent::resetSessions();
     }
 
     public function testInvalidTimeoutSettingThrowsException(): void
     {
-        $this->expectException(DriverException::class);
         $this->getSession()->start();
+        $driver = $this->getDriver();
 
-        $this->getSession()->getDriver()->setTimeouts(['invalid' => 0]);
+        $this->expectException(DriverException::class);
+
+        $driver->setTimeouts(['invalid' => 0]);
     }
 
     public function testShortTimeoutDoesNotWaitForElementToAppear(): void
     {
-        $this->getSession()->getDriver()->setTimeouts(['implicit' => 0]);
+        $this->getDriver()->setTimeouts(['implicit' => 0]);
 
         $this->getSession()->visit($this->pathTo('/js_test.html'));
 
@@ -48,12 +51,18 @@ class TimeoutTest extends TestCase
 
     public function testLongTimeoutWaitsForElementToAppear(): void
     {
-        $this->getSession()->getDriver()->setTimeouts(['implicit' => 5000]);
+        $this->getDriver()->setTimeouts(['implicit' => 5000]);
 
         $this->getSession()->visit($this->pathTo('/js_test.html'));
         $this->findById('waitable')->click();
         $element = $this->getSession()->getPage()->find('css', '#waitable > div');
 
         $this->assertNotNull($element);
+    }
+
+    private function getDriver(): WebdriverClassicDriver
+    {
+        /** @phpstan-ignore-next-line */
+        return $this->getSession()->getDriver();
     }
 }
