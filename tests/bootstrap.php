87b6a07e49de9f8b36282d3ba5c531bf6cc37ebf
Christian Sciberras <christian@sciberras.me>
2024-11-01 17:51:25 +0100

    Use port from host var

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,10 +4,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\Process\Process;
 
+$minkTestServerPort = isset($_SERVER['WEB_FIXTURES_HOST'])
+    ? parse_url($_SERVER['WEB_FIXTURES_HOST'], PHP_URL_PORT)
+    : '8002';
+
 $minkTestServer = new Process([
     PHP_BINARY,
     '-S',
-    '0.0.0.0:8002',
+    "0.0.0.0:$minkTestServerPort",
     '-t',
     __DIR__ . '/../vendor/mink/driver-testsuite/web-fixtures'
 ]);
