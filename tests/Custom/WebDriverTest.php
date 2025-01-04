5a53ec5995c7a670fa87d214f13627c3d18c4771
Christian Sciberras <christian@sciberras.me>
2025-01-04 14:41:09 +0100

    Add string type assertion test

--- tests/Custom/WebDriverTest.php
+++ tests/Custom/WebDriverTest.php
@@ -3,6 +3,10 @@
 namespace Mink\WebdriverClassicDriver\Tests\Custom;
 
 use Behat\Mink\Exception\DriverException;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\RemoteWebElement;
+use Facebook\WebDriver\WebDriverOptions;
+use Facebook\WebDriver\WebDriverTimeouts;
 use Mink\WebdriverClassicDriver\WebdriverClassicDriver;
 
 class WebDriverTest extends TestCase
@@ -71,5 +75,33 @@ class WebDriverTest extends TestCase
             $this->getConfig()->getBrowserName(),
             $this->driver->getBrowserName()
         );
+    }
+
+    public function testThatDriverCatchesUnexpectedAttributeValueType(): void
+    {
+        $mockWebDriver = $this->createMock(RemoteWebDriver::class);
+        $mockWebDriverOptions = $this->createMock(WebDriverOptions::class);
+        $mockWebDriverTimeouts = $this->createMock(WebDriverTimeouts::class);
+        $mockElement = $this->createMock(RemoteWebElement::class);
+        $mockWebDriver->method('manage')->willReturn($mockWebDriverOptions);
+        $mockWebDriverOptions->method('timeouts')->willReturn($mockWebDriverTimeouts);
+        $mockWebDriver
+            ->expects($this->once())
+            ->method('findElement')
+            ->willReturn($mockElement);
+        $mockWebDriver
+            ->expects($this->once())
+            ->method('executeScript')
+            ->with('return arguments[0].getAttribute("some-attribute")', [$mockElement])
+            ->willReturn(['invalid attribute value']);
+
+        $driver = new WebdriverClassicDriver('fake browser', [], 'example.com', fn() => $mockWebDriver);
+
+        $driver->start();
+
+        $this->expectException(DriverException::class);
+        $this->expectExceptionMessage('The element\'s some-attribute attribute should be a string or at least a scalar value, but received `array` instead');
+
+        $driver->getAttribute('//fake', 'some-attribute');
     }
 }
